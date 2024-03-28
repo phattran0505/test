4c72f8ef36a7e7275c06c2ee9f24157339211b41
phattran <phattran052004@gmail.com>
2024-03-28 23:22:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(4000,(req,res)=>{
+    console.log("connect server success");
+})
