0eed626e2354adbb7b8af3fc1efd933bc4f7e1f7
phattran <phattran052004@gmail.com>
2024-03-28 23:25:50 +0700

    change route

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+app.get("/about",(req,res)=>{
+    res.send("hello world !!!")
 })
 
 app.listen(4000,(req,res)=>{
